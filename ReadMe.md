002d4cf4e0f9e8b8d6772baf3098119c9c721222
David M.T <thigedavidmaitho@gmail.com>
2022-12-29 19:29:26 +0300

    setup lighsail sdk interaction

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# Roaster Demo Instance Spinner
+
+## Running Localy without Docker
+
+- `AWS_PROFILE=bellwether npm run dev`
+- Build - `npm run build`
--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
+// ----------LIGHTSAIL --------------
+import * as AWS from "@aws-sdk/client-lightsail";
+const lightsail = new AWS.Lightsail({ region: "us-east-1" });
+// ----------END LIGHTSAIL --------------
+
 dotenv.config();
 
 const app: Express = express();
@@ -10,6 +15,21 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server is Running');
 });
 
+// ----------LIGHTSAIL --------------
+function listInstances(){
+  console.log("List LS Instances")
+
+  const params = {
+    serviceName: 'roaster-app-demo-service'
+  };
+  lightsail.getContainerServices(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+}
+// ----------END LIGHTSAIL --------------
+
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  listInstances()
 });
